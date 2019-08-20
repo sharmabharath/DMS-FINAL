4ffe22d7a9e519b5cea9d61f0292beaa90ce9841
bharath sharma <sharmabharath1998@gmail.com>
2019-08-20 22:18:13 +0530

    added login func

---
+++ script.js
@@ -0,0 +1,22 @@
+var logbtn =document.getElementById('lgn');
+logbtn.addEventListener("click",function(){
+
+//window.open("dashboard.html");
+if ( (document.getElementById('uid').value)== "admin" && (document.getElementById('pid').value) == "1234") {
+window.open("dashboard.html");
+}
+else
+{
+	alert("wrong credentials!!!");
+}
+
+//console.log(" login pe clicking");
+})
+
+
+var logbtn =document.getElementById('fgt');
+logbtn.addEventListener("click",function(){
+//yaha 
+
+alert("needs database");
+})
